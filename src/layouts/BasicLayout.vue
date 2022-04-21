f4b79916dce22ada52961f0ab5fffc6d8093694e
Sendya <18x@loacg.com>
2022-04-21 16:17:47 +0800

    fix: layout

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -55,7 +55,7 @@ const state = reactive<Omit<RouteContextProps, 'menuData'>>({
 });
 const loading = ref(false);
 const proConfig = ref({
-  layout: 'side',
+  layout: 'mix',
   navTheme: 'light',
   fixedHeader: true,
   fixSiderbar: true,
--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,5 +1,5 @@
 <template>
-  <PageContainer ghost>
+  <PageContainer>
     <a-result
       status="404"
       :style="{
