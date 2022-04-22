c1585c89fc703b29ae78355ec86b94b88c46dbee
Sendya <18x@loacg.com>
2022-04-22 14:14:59 +0800

    fix: add report bundle

--- vite.config.ts
+++ vite.config.ts
@@ -8,54 +8,60 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import OptimizationPersist from 'vite-plugin-optimize-persist';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      dts: 'src/auto-imports.d.ts',
-      imports: ['vue', 'vue-router'],
+export default defineConfig(() => {
+  const lifecycle = process.env.npm_lifecycle_event;
 
-      eslintrc: {
-        enabled: true,
-        filepath: './.eslintrc-auto-import.json',
-        globalsPropValue: true,
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        dts: 'src/auto-imports.d.ts',
+        imports: ['vue', 'vue-router'],
+
+        eslintrc: {
+          enabled: true,
+          filepath: './.eslintrc-auto-import.json',
+          globalsPropValue: true,
+        },
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+        deep: true,
+        dirs: ['src/components'],
+        extensions: ['vue', 'tsx'],
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false,
+          }),
+        ],
+      }),
+      PkgConfig(),
+      OptimizationPersist(),
+      lifecycle === 'report' ? visualizer({ open: true, brotliSize: true, filename: 'report.html' }) : null,
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-      deep: true,
-      dirs: ['src/components'],
-      extensions: ['vue', 'tsx'],
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false,
-        }),
-      ],
-    }),
-    PkgConfig(),
-    OptimizationPersist(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        // DO NOT REMOVE THIS LINE
-        javascriptEnabled: true,
-        modifyVars: {
-          // hack: `true; @import 'ant-design-vue/dist/antd.variable.less'`,
-          // '@primary-color': '#eb2f96', // 全局主色
+    css: {
+      preprocessorOptions: {
+        less: {
+          // DO NOT REMOVE THIS LINE
+          javascriptEnabled: true,
+          modifyVars: {
+            // hack: `true; @import 'ant-design-vue/dist/antd.variable.less'`,
+            // '@primary-color': '#eb2f96', // 全局主色
+          },
         },
       },
     },
-  },
-  optimizeDeps: {
-    include: ['@ant-design/icons-vue', 'ant-design-vue'],
-  },
+    optimizeDeps: {
+      include: ['@ant-design/icons-vue', 'ant-design-vue'],
+    },
+  };
 });
